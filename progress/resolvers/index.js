f2041a99ce0b6fce35ec753ac41a00ad2291f8c4
Nik Graf <nik@nikgraf.com>
2020-03-18 20:49:01 +0100

    add progress

--- progress/resolvers/index.js
+++ progress/resolvers/index.js
@@ -1,5 +1,6 @@
 const { getProducts, createProduct, getProduct } = require("../db/products");
 const { login, updateUser } = require("../db/users");
+const { UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -10,7 +11,17 @@ module.exports = {
       return login(args.input.email, args.input.password);
     },
     addProductToCart: (_parent, args) => {
+      // verfiy productId -> UserInputError
+      // verfiy amount is smaller than the product stock -> UserInputError
       return { amount: args.input.amount };
+    },
+    createProduct: (_parent, args, context) => {
+      if (args.input.name.length < 3) {
+        throw new UserInputError("Form Arguments invalid", {
+          invalidArgs: ["input.name"]
+        });
+      }
+      return { product: createProduct(args.input) };
     }
   }
 };
