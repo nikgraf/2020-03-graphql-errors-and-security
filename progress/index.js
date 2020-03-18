713adf2eee70e0e6a7d61af2f02eaedd03720e75
Nik Graf <nik@nikgraf.com>
2020-03-18 21:23:44 +0100

    add errors

--- progress/index.js
+++ progress/index.js
@@ -2,10 +2,18 @@ const { ApolloServer } = require("apollo-server");
 const { importSchema } = require("graphql-import");
 const resolvers = require("./resolvers");
 const typeDefs = importSchema("./schema.graphql");
+const { getUserByToken } = require("./db/users");
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => {
+    const token = req.headers.authorization
+      ? req.headers.authorization.replace("Bearer ", "")
+      : undefined;
+    const user = getUserByToken(token);
+    return { user, meaningOfLife: 42 };
+  }
 });
 
 server.listen().then(({ url }) => {
--- progress/resolvers/index.js
+++ progress/resolvers/index.js
@@ -1,21 +1,46 @@
 const { getProducts, createProduct, getProduct } = require("../db/products");
 const { login, updateUser } = require("../db/users");
-const { UserInputError } = require("apollo-server");
+const {
+  UserInputError,
+  AuthenticationError,
+  ForbiddenError
+} = require("apollo-server");
 
 module.exports = {
   Query: {
     products: () => getProducts()
   },
   Mutation: {
-    login: (_parent, args) => {
+    login: (_parent, args, context) => {
       return login(args.input.email, args.input.password);
     },
     addProductToCart: (_parent, args) => {
-      // verfiy productId -> UserInputError
-      // verfiy amount is smaller than the product stock -> UserInputError
+      const product = getProduct(args.input.productId);
+      if (!product) {
+        throw new UserInputError("Product doesn't exist", {
+          invalidArgs: ["input.productId"]
+        });
+      }
+      if (product.stock < args.input.amount) {
+        throw new UserInputError(
+          "Not enough items in stock for this product.",
+          {
+            invalidArgs: ["input.amount"]
+          }
+        );
+      }
       return { amount: args.input.amount };
     },
     createProduct: (_parent, args, context) => {
+      if (!context.user) {
+        throw new AuthenticationError(
+          "User must be logged in to create a Product."
+        );
+      }
+      if (context.user.role !== "admin") {
+        throw new ForbiddenError("Only Admins can create products.");
+      }
+
       if (args.input.name.length < 3) {
         throw new UserInputError("Form Arguments invalid", {
           invalidArgs: ["input.name"]
